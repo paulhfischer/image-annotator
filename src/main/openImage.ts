c4b6f0101029eb059e7c7da2d99f6c87cb3c401d
Paul Fischer <paul.fischer@mailbox.org>
2023-12-13 23:27:51 +0100

    resize and convert images on load

--- src/main/openImage.ts
+++ src/main/openImage.ts
@@ -1,7 +1,8 @@
 import { dialog } from 'electron';
-import { readFileSync } from 'fs';
-import sizeOf from 'image-size';
 import { basename } from 'path';
+import sharp from 'sharp';
+
+const IMAGE_SIZE = 2000;
 
 export interface ImageMeta {
     path: string;
@@ -11,10 +12,32 @@ export interface ImageMeta {
     height: number;
 }
 
+const resizeImage = async (image: sharp.Sharp): Promise<sharp.Sharp> => {
+    const { width, height } = await image.metadata();
+
+    if (!width || !height) throw new Error();
+
+    let resizedImage = image;
+    if (width > IMAGE_SIZE || height > IMAGE_SIZE) {
+        resizedImage = image.resize({
+            width: IMAGE_SIZE,
+            height: IMAGE_SIZE,
+            fit: sharp.fit.inside,
+        });
+
+        dialog.showMessageBox({
+            message: `Image has been downsized!`,
+            type: 'info',
+        });
+    }
+
+    return sharp(await resizedImage.toBuffer());
+};
+
 export const openImage = async (): Promise<ImageMeta | undefined> => {
     const { filePaths, canceled } = await dialog.showOpenDialog({
         properties: ['openFile'],
-        filters: [{ name: 'Images', extensions: ['jpg', 'jpeg'] }],
+        filters: [{ name: 'Images', extensions: ['jpg', 'jpeg', 'png', 'gif', 'tiff'] }],
     });
 
     if (canceled) {
@@ -22,8 +45,14 @@ export const openImage = async (): Promise<ImageMeta | undefined> => {
     }
     const path = filePaths[0];
     const filename = basename(path);
-    const content = readFileSync(path, { encoding: 'base64' });
-    const { width, height } = sizeOf(path);
+
+    let image = sharp(path).jpeg({
+        quality: 100,
+        mozjpeg: true,
+    });
+    image = await resizeImage(image);
+    const { width, height } = await sharp(await image.toBuffer()).metadata();
+    const content = (await image.toBuffer()).toString('base64');
 
     if (!width || !height) throw new Error();
 
